1d564fc72ab08cd3c388d8f26f5bbf81bcb25d7c
Jendoliver <alvarezmonllor@gmail.com>
2018-04-05 01:43:04 +0200

    Jukebox interface created

---
+++ src/main/java/com/apporelbotna/gameserver/launcher/audio/Jukebox.java
@@ -0,0 +1,14 @@
+package com.apporelbotna.gameserver.launcher.audio;
+
+/**
+ * Puro heavy el metal
+ *
+ * @author Jendoliver
+ *
+ */
+public interface Jukebox
+{
+	void play();
+	void loop();
+	void stop();
+}
